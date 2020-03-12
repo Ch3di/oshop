97b7a801c28c0ad6ea5345587e8bfb3a067091a6
chady <chedibouhlel@gmail.com>
2020-03-12 18:33:45 +0100

    added Bootstrap and firebase on the config files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCK0qQ10TsIi8z6Viog-cSdoyG4javFtmI',
+    authDomain: 'oshop-de817.firebaseapp.com',
+    databaseURL: 'https://oshop-de817.firebaseio.com',
+    projectId: 'oshop-de817',
+    storageBucket: 'oshop-de817.appspot.com',
+    messagingSenderId: '709695528094',
+    appId: '1:709695528094:web:12c2327517643a0ce22f8f',
+    measurementId: 'G-LN1TK3H32R'
+  }
 };
